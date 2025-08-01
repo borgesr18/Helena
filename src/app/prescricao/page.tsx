ccb1bb2f28c93e5761fb0fa52fbc233d18d92dee
borgesr18 <borgesr18@yahoo.com.br>
2025-08-01 13:59:21 -0300

    Create page.tsx

---
+++ src/app/prescricao/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import React from 'react'
+import PrescriptionLayout from '@/components/prescricao/PrescriptionLayout'
+
+export default function PrescricaoPage() {
+  const paciente = 'Rodrigo Borges de Amorim'
+  const medicamento = 'Dipirona 500mg'
+  const posologia = '1 comprimido de 8 em 8 horas por 5 dias'
+  const observacoes = 'Tomar após as refeições'
+  const data = new Date().toLocaleDateString()
+
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <PrescriptionLayout
+        paciente={paciente}
+        medicamento={medicamento}
+        posologia={posologia}
+        observacoes={observacoes}
+        data={data}
+      />
+    </div>
+  )
+}
