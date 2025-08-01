71c5ec140fba48fa2aa38441d0eecdb649cb70d8
borgesr18 <borgesr18@yahoo.com.br>
2025-08-01 08:09:30 -0300

    Update MainLayout.tsx

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -37,7 +37,7 @@ export function MainLayout({ children }: { children: React.ReactNode }) {
     <div className="bg-helena-light min-h-screen">
       <Header />
       <Sidebar />
-      <main className="ml-64 pt-20 p-6">
+      <main className="ml-64 pt-22 p-6">
         {children}
       </main>
     </div>
